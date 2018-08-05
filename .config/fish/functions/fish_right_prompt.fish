44fd40c7077bdb1637f951a0e555c13f027fafac
Matthew Vilim <mvilim@stanford.edu>
2018-08-04 22:47:48 -0700

    i

---
+++ .config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,9 @@
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_showdirtystate "yes"
+set __fish_git_prompt_showstashstate "yes"
+set __fish_git_prompt_showuntrackedfiles "yes"
+set __fish_git_prompt_showupstream "yes"
+
+function fish_right_prompt
+  __fish_git_prompt
+end
