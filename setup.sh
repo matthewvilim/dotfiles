a3f681025e8078d4360b23f2c482257a76e61a73
Matthew Vilim <mvilim@stanford.edu>
2018-10-31 22:39:59 -0700

    i

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 dir=~/dotfiles
-files=".bash_profile .bashrc .ctags .dir_colors .gitconfig .inputrc .tmux.conf .vimrc .xinitrc .Xmodmap .config/alacritty/alacritty.yml .config/i3/config .config/i3/i3status.conf .config/redshift.conf"
+files=".ctags .gitconfig .inputrc .tmux.conf .vimrc .xinitrc .Xmodmap .config/fish/config.fish .config/alacritty/alacritty.yml .config/i3/config .config/i3/i3status.conf .config/redshift.conf"
 
 for file in $files; do
   ln -sf ~/dotfiles/$file ~/$file
