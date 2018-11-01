075acb438bd0d91b238d0d328a92dadabc2e6933
Matthew Vilim <mvilim@stanford.edu>
2018-11-01 12:20:53 -0700

    i

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,17 +3,17 @@ set -g fish_key_bindings fish_vi_key_bindings
 
 set -Ux EDITOR vim
 
-set LM_LICENSE_FILE 7195@cadlic0.stanford.edu
-set LM_LICENSE_FILE 27000@cadlic0.stanford.edu $LM_LICENSE_FILE
-set VCS_HOME /cad/synopsys/vcs/K-2015.09-SP2-7
-set PATH $HOME/fish-shell $HOME/sbt/bin $VCS_HOME/amd64/bin $PATH
-set PIR_HOME $HOME/pir
-set SPATIAL_HOME $HOME/spatial-lang
-set PLASTICINE_HOME $HOME/plasticine
-set _JAVA_OPTIONS "-Xmx32g -Xss8912k -Xms16g"
+set -Ux LM_LICENSE_FILE 7195@cadlic0.stanford.edu
+set -Ux LM_LICENSE_FILE 27000@cadlic0.stanford.edu $LM_LICENSE_FILE
+set -Ux VCS_HOME /cad/synopsys/vcs/K-2015.09-SP2-7
+set -Ux PATH $HOME/.local/bin $HOME/fish-shell $HOME/sbt/bin $VCS_HOME/amd64/bin $PATH
+set -Ux PIR_HOME $HOME/pir
+set -Ux SPATIAL_HOME $HOME/spatial-lang
+set -Ux PLASTICINE_HOME $HOME/plasticine
+set -Ux _JAVA_OPTIONS "-Xmx32g -Xss8912k -Xms16g"
 
 set XILINX_VIVADO /opt/Xilinx/Vivado/2017.1
-set PATH $XILINX_VIVADO/bin $PATH
+set -Ux PATH $XILINX_VIVADO/bin $PATH
 #alias zycp 'scp $(basename $(pwd)).tar.gz mvilim@holodeck-zc706:'
 alias zynq 'ssh mvilim@holodeck-zc706'
 
