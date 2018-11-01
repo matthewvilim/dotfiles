e584b567da4d6d8c7dd37d44907146ad8770dc8f
Matthew Vilim <mvilim@stanford.edu>
2018-11-01 09:29:35 -0700

    i

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -59,3 +59,26 @@ function fish_prompt --description 'Write out the prompt'
         printf '%s%s@%s%s%s(%s)%s%s%s> ' (set_color yellow) (whoami) (set_color purple) (prompt_hostname) (set_color white) (echo $WINDOW) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
     end
 end
+
+set -l color00 '#002b36'
+set -l color01 '#073642'
+set -l color02 '#586e75'
+set -l color03 '#657b83'
+set -l color04 '#839496'
+set -l color05 '#93a1a1'
+set -l color06 '#eee8d5'
+set -l color07 '#fdf6e3'
+set -l color08 '#dc322f'
+set -l color09 '#cb4b16'
+set -l color0A '#b58900'
+set -l color0B '#859900'
+set -l color0C '#2aa198'
+set -l color0D '#268bd2'
+set -l color0E '#6c71c4'
+set -l color0F '#d33682'
+
+set -Ux FZF_DEFAULT_OPTS "
+  --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D
+  --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
+  --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
+"
