6b029015876aa4b79b047615cfe292d3b9d24dcb
Matthew Vilim <mvilim@stanford.edu>
2020-09-23 09:18:40 -0700

    i

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 dir=~/dotfiles
-files=".bash_aliases .gdbinit .gitconfig .inputrc .xinitrc .config/alacritty/alacritty.yml .config/i3/config .config/i3/i3status.conf .config/redshift.conf"
+files=".bash_aliases .gdbinit .gitconfig .inputrc .xinitrc .config/alacritty/alacritty.yml .config/i3/config .config/i3/i3status.conf .config/redshift.conf .config/nvim/init.vim .config/nvim/coc-settings.json"
 
 for file in $files; do
   ln -sf ~/dotfiles/$file ~/$file
